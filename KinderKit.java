32cee0d0b70f3e334a1279058b41d475996b0b45
fschnizzle <fschneider@student.unimelb.edu.au>
2023-03-27 13:40:39 +1100

    Welcome Message display

--- KinderKit.java
+++ KinderKit.java
@@ -8,5 +8,16 @@ import java.util.Scanner;
  */
 public class KinderKit {
     public static void main(String[] args) {
+        // Initialise command line settings
+        int canvasWidth = Integer.parseInt(args[0]);
+        int canvasHeight = Integer.parseInt(args[1]);
+
+        // Display Welcome Message
+        System.out.println("----DIGITAL KINDER KIT: LET'S PLAY & LEARN----\n" + "Current drawing canvas settings:");
+
+        System.out.println( // Display initial settings based on command line inputs
+                String.format("- Width: %d\n- Height: %d\n- Background character: -", canvasWidth, canvasHeight));
+
+        // Display Main Menu
     }
 }
