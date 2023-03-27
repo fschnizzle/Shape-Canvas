b9c622b6453d0e0d089bc39d03de48ca84e68f02
fschnizzle <fschneider@student.unimelb.edu.au>
2023-03-27 21:24:02 +1100

    implement DrawingCanvas class
    Including method canvasString() which generates the string required to display the canvas visually

--- DrawingCanvas.java
+++ DrawingCanvas.java
@@ -1,4 +1,82 @@
 import java.util.Scanner;
 
 public class DrawingCanvas {
+    // Instance Variables
+    private int width;
+    private int height;
+    private char bgChar;
+
+    // Constructors
+    public DrawingCanvas(int width, int height, char bgChar) {
+        setWidth(width);
+        setHeight(height);
+        setBgChar(bgChar);
+
+        String canvas = canvasString();
+    }
+
+    public DrawingCanvas(int width, int height) {
+        setWidth(width);
+        setHeight(height);
+        setBgChar();
+
+        String canvas = canvasString();
+    }
+
+    // Accessors
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char getBgChar() {
+        return bgChar;
+    }
+
+    // Mutators
+    public void setWidth(int width) {
+        if (width >= 0) {
+            this.width = width;
+        } else {
+            System.err.println("Invalid width value!");
+        }
+    }
+
+    public void setHeight(int height) {
+        if (height >= 0) {
+            this.height = height;
+        } else {
+            System.err.println("Invalid height value!");
+        }
+    }
+
+    public void setBgChar(char bgChar) {
+        this.bgChar = bgChar;
+    }
+
+    public void setBgChar() {
+        this.bgChar = '-'; // Default background character
+    }
+
+    // canvasString() method. Creates the character string that visualises an empty
+    // canvas.
+    public String canvasString() {
+        String Canvas = ""; // Initialise empty canvas string
+
+        // Add to canvas row by row
+        for (int y = 0; y < height; y++) {
+            // Add to canvas char by char
+            for (int x = 0; x < width; x++) {
+                Canvas = Canvas + bgChar;
+            }
+            // Move to next line
+            Canvas = Canvas + "\n";
+        }
+
+        return Canvas;
+    }
+
 }
