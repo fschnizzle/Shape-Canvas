aa386e79a4c9e1eec176d47fee4971f71ceb2134
fschnizzle <fschneider@student.unimelb.edu.au>
2023-03-27 16:56:08 +1100

    Implement main menu loop

--- KinderKit.java
+++ KinderKit.java
@@ -19,5 +19,31 @@ public class KinderKit {
                 String.format("- Width: %d\n- Height: %d\n- Background character: -", canvasWidth, canvasHeight));
 
         // Display Main Menu
+        Scanner keyboard = new Scanner(System.in);
+        boolean exitMenu = false;
+        while (!exitMenu) {
+            // Prompts user for menu selection
+            System.out.println("Please select an option. Type 4 to exit.\n" +
+                    "1. Draw triangles\n2. Draw rectangles\n3. Update drawing canvas settings\n4. Exit");
+
+            // Takes input
+            int menuCase = Integer.parseInt(keyboard.nextLine());
+
+            // Switch for menu items (cases: 1-4)
+            switch (menuCase) {
+                case 1:
+                    // Draw Triangle
+                case 2:
+                    // Draw Rectangle
+                case 3:
+                    // Update Canvas Settings
+                case 4:
+                    exitMenu = true;
+                    break;
+            }
+            // System.out.println("Selected: " + menuCase); // Debugging purposes
+
+        }
+
     }
 }
