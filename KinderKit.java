eb852c7d4541b2aa29e798cac9bef22866ece103
fschnizzle <fschneider@student.unimelb.edu.au>
2023-03-27 13:16:31 +1100

    File Initialisation

---
+++ KinderKit.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * COMP90041, Sem1, 2023: Assignment 1
+ * Date: 27/03/23
+ * 
+ * @author: Flynn Schneider
+ */
+public class KinderKit {
+    public static void main(String[] args) {
+    }
+}
