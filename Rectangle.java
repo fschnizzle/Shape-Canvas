4c80de6e58a689ace3f7ae924aeefc45d6014647
fschnizzle <fschneider@student.unimelb.edu.au>
2023-04-07 10:58:12 +1000

    Update rectangle documentation and styling

--- Rectangle.java
+++ Rectangle.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 public class Rectangle {
-    // Scanner keyboard = new Scanner(System.in);
 
     // Instance Variables
-    private int height;
-    private int width;
-    private char pChar;
-    private int xPosition;
-    private int yPosition;
+    private int height; // height of the rectangle
+    private int width; // width of the rectangle
+    private char pChar; // printing character of the rectangle
+    private int xPosition; // starting X position of the rectangle
+    private int yPosition; // starting Y position of the rectangle
     private Scanner keyboard;
 
     // Constructors
@@ -17,12 +16,6 @@ public class Rectangle {
         setWidth();
         setPChar();
         this.keyboard = kboard;
-    }
-
-    public Rectangle(int height, int width, char pChar) {
-        setHeight(height);
-        setWidth(width);
-        setPChar(pChar);
     }
 
     // Accessors
@@ -87,43 +80,51 @@ public class Rectangle {
         this.yPosition = yPosition;
     }
 
-    // METHODS
+    // Methods
 
+    // Method that allows a user to move this rectangle on a given DrawingCanvas in
+    // any direction if it remains entirely on the canvas
     public void moveRectangle(DrawingCanvas canvas, int displaceX, int displaceY) {
 
-        String selection = "a";
-        while (selection.equals("a") || selection.equals("s")
-                || selection.equals("w") || selection.equals("z")) {
-            //
+        String selection;
+        // do-while loop until user selects other than {'a','s','w','z'}
+        do {
+            // Update X & Y position
             this.setXPosition(displaceX);
             this.setYPosition(displaceY);
+
+            // Display canvas with rectangle
             System.out.print(canvas.canvasString(this));
+
+            // Prompt user for left, right, up, down (A,S,W,Z) input
             System.out.println(
                     "Type A/S/W/Z to move left/right/up/down. Use other keys to go back to the Zooming/Moving menu.");
             selection = keyboard.nextLine();
+
+            // Moves rectangle according to selection using switch cases
             switch (selection.toLowerCase()) {
-                case "a":
+                case "a": // Move left case
                     if (displaceX == 0) {
                         System.out.println("You cannot move this rectangle outside of the drawing canvas!");
                     } else {
                         displaceX--;
                     }
                     break;
-                case "s":
+                case "s": // Move right case
                     if (displaceX + this.getWidth() == canvas.getWidth()) {
                         System.out.println("You cannot move this rectangle outside of the drawing canvas!");
                     } else {
                         displaceX++;
                     }
                     break;
-                case "w":
+                case "w": // Move up case
                     if (displaceY == 0) {
                         System.out.println("You cannot move this rectangle outside of the drawing canvas!");
                     } else {
                         displaceY--;
                     }
                     break;
-                case "z":
+                case "z": // Move down case
                     if (displaceY + this.getHeight() == canvas.getHeight()) {
                         System.out.println("You cannot move this rectangle outside of the drawing canvas!");
                     } else {
@@ -133,60 +134,70 @@ public class Rectangle {
                 default:
             }
 
-        }
+        } while (selection.equals("a") || selection.equals("s")
+                || selection.equals("w") || selection.equals("z"));
 
     }
 
+    // Method that allows a user to scale this rectangle by one
+    // in both dimensions if it remains entirely on the canvas
     public void zoomRectangle(DrawingCanvas canvas, int displaceX, int displaceY) {
 
-        String selection = "i";
-        while (selection.equals("i") || selection.equals("o")) {
-            //
+        String selection;
+        // do-while loop until user selects other than {'i','o'}
+        do {
+            // Update X & Y position
             this.setXPosition(displaceX);
             this.setYPosition(displaceY);
+
+            // Prompt user for zoom in, out (I,O) input
             System.out.print(canvas.canvasString(this));
             System.out.println(
                     "Type I/O to zoom in/out. Use other keys to go back to the Zooming/Moving menu.");
             selection = keyboard.nextLine();
+
+            // Resizes triangle according to selection using switch cases
             switch (selection.toLowerCase()) {
-                case "i":
+                case "i": // Zoom in case
                     if (displaceX + this.getWidth() == canvas.getWidth()
                             || displaceY + this.getHeight() == canvas.getHeight()) {
                         System.out.println("This rectangle reaches its limit. You cannot make it bigger!");
                     } else {
+                        // Increment height and width
                         this.setHeight(this.getHeight() + 1);
                         this.setWidth(this.getWidth() + 1);
                     }
                     break;
-                case "o":
+                case "o": // Zoom in case
                     if (this.getHeight() == 1 || this.getWidth() == 1) {
                         System.out.println("This rectangle reaches its limit. You cannot make it smaller!");
                     } else {
+                        // Decrement height and width
                         this.setHeight(this.getHeight() - 1);
                         this.setWidth(this.getWidth() - 1);
                     }
                     break;
                 default:
             }
-
-        }
-
+        } while (selection.equals("i") || selection.equals("o"));
     }
 
-    public void RectangleInputs(DrawingCanvas canvas) {
+    public void rectangleInputs(DrawingCanvas canvas) {
 
-        String selection = "Y";
-
-        while (selection.toUpperCase().equals("Y")) {
-            // Starting Coordinates
+        String selection;
+        // do-while loop until user selects other than 'Y'
+        do {
+            // Initialise starting Coordinates
             int displaceX = 0;
             int displaceY = 0;
 
-            // Rectangle properties prompt and error detection
+            // Rectangle dimensions prompt and error detection
             // Check width
             do {
                 System.out.print("width:\n");
                 setWidth(Integer.parseInt(keyboard.nextLine()));
+
+                // Error detection to ensure triangle is within canvas
                 if (this.getWidth() > canvas.getWidth()) {
                     System.out.println(String.format(
                             "Error! The width is too large (Current canvas size is %dx%d). Please try again.",
@@ -207,46 +218,47 @@ public class Rectangle {
 
             } while (this.getHeight() > canvas.getHeight());
 
-            this.setXPosition(displaceX);
-            this.setYPosition(displaceY);
-
+            // Prompts user for printing character
             System.out.print("Printing character:\n");
             setPChar(keyboard.nextLine().charAt(0));
 
-            // Zoom and Move display / prompt loop
-            selection = "Y";
-            while (selection.toUpperCase().equals("Y") || selection.toUpperCase().equals("Z")
-                    || selection.toUpperCase().equals("M")) {
+            // Update coordinates
+            this.setXPosition(displaceX);
+            this.setYPosition(displaceY);
 
+            // Zoom and Move display / prompt loop
+            do {
+                // Get initial coordinates
                 displaceX = this.getXPosition();
                 displaceY = this.getYPosition();
-                // Display Canvas with Triangle
 
+                // Display Canvas with Triangle
                 System.out.print(canvas.canvasString(this));
 
                 // Prompt user for Zoom / Move controls
                 System.out.println("Type Z/M for zooming/moving. Use other keys to quit the Zooming/Moving mode.");
                 selection = keyboard.nextLine();
+
+                // Enter move or zoom functionality according to selection
                 switch (selection.toLowerCase()) {
                     case "m":
-                        // System.out.println("MOVE TRIANGLE SELECTED");
+                        // Enter move mode
                         moveRectangle(canvas, displaceX, displaceY);
-                        // Implement MOVE method
                         break;
                     case "z":
+                        // Enter zoom mode
                         zoomRectangle(canvas, displaceX, displaceY);
-                        // Implement Zoom method
                         break;
                     default:
                 }
-            }
-
-            // System.out.print(canvas.canvasString(this));
+            } while (selection.toUpperCase().equals("Z")
+                    || selection.toUpperCase().equals("M"));
 
             // Create new rectangle selection prompt
             System.out.println("Draw another rectangle (Y/N)?");
             selection = keyboard.nextLine();
-        }
+
+        } while (selection.toUpperCase().equals("Y"));
 
     }
 }
